e2b9da959bbf16a09b39bbe19da0611ad10da2df
Dilson Pereira <dilsonpereira231@gmail.com>
2025-06-26 22:37:12 -0300

    fix: corrigido caminho e importação para Render

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install uvicorn  # Garante que uvicorn esteja instalado no ambiente global
-cd app
+pip install uvicorn
+cd backend/app
 uvicorn main:app --host 0.0.0.0 --port 10000
